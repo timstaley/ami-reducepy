94b205a8a9674c01f63aa04542f0fe1710a84e0f
Tim Staley <tim.staley@soton.ac.uk>
2013-09-26 15:11:49 +0100

    Update standard reduction script.

--- driveami/scripts.py
+++ driveami/scripts.py
@@ -3,23 +3,28 @@ r"""
 version
 flag all
 flag int \
+flag amp field no \
+flag data \
+update pcal \
 subtract modmeans \
 subtract zeros \
 subtract means \
-update pcal \
 fft \
 frotate forward y n \
-flag amp field yes no 1 \
 apply rain \
-flag amp field no 1.0 1 \
-flag amp field no 0.225 20 \
+flag amp field no 0.19 1 yes \
+flag amp field no 0.03 30 yes \
+flag amp field yes no 60 \
 frotate forward n y \
 cal inter \
 reweight \
 show flagging no yes \
-smooth 50 \
+smooth 20 \
 show flagging no yes \
-flag bad cal all all all \
+flag bad cal \
+flag amp field no 0.05 1 yes \
+smooth 10 \
+flag amp field no 0.015 1 yes \
 scan dat cal yes \
 scan dat field yes \
 show flagging no yes \
