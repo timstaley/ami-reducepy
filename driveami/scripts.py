67dd039f00245c1de18ec08259f483d920052a63
Tim Staley <tim.staley@astro.ox.ac.uk>
2015-02-18 13:40:43 +0000

    Raise amplitude thresholds for flagging processes in standard script.
    Gemma's tests indicate these values are more suitable for a wide
    range of obs.

--- driveami/scripts.py
+++ driveami/scripts.py
@@ -12,8 +12,8 @@ subtract means \
 fft \
 frotate forward y n \
 apply rain \
-flag amp field no 0.19 1 yes \
-flag amp field no 0.03 30 yes \
+flag amp field no 0.95 1 yes \
+flag amp field no 0.15 30 yes \
 flag amp field yes no 60 \
 frotate forward n y \
 cal inter \
@@ -21,10 +21,10 @@ reweight \
 show flagging no yes \
 smooth 20 \
 show flagging no yes \
-flag bad cal \
-flag amp field no 0.05 1 yes \
+flag bad calibrator \
+flag amp field no 0.25 1 yes \
 smooth 10 \
-flag amp field no 0.015 1 yes \
+flag amp field no 0.075 1 yes \
 scan dat cal yes \
 scan dat field yes \
 show flagging no yes \
@@ -44,8 +44,8 @@ subtract means \
 fft \
 frotate forward y n \
 apply rain \
-flag amp  no 0.19 1 yes \
-flag amp  no 0.03 30 yes \
+flag amp  no 0.95 1 yes \
+flag amp  no 0.15 30 yes \
 flag amp  yes no 60 \
 frotate forward n y \
 cal inter \
@@ -54,9 +54,9 @@ show flagging no yes \
 smooth 20 \
 show flagging no yes \
 flag bad \
-flag amp no 0.05 1 yes \
+flag amp no 0.25 1 yes \
 smooth 10 \
-flag amp  no 0.015 1 yes \
+flag amp  no 0.075 1 yes \
 scan dat yes \
 show flagging no yes \
 """
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = ['pexpect',
 
 setup(
     name="drive-ami",
-    version="0.7.6",
+    version="0.7.7",
     packages=['driveami'],
     scripts=['bin/driveami_filter_rawfile_listing.py',
              'bin/driveami_list_rawfiles.py',
